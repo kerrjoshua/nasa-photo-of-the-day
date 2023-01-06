7e1440458f01e23028b6e2df1b4501ec1d30eb3e
Joshua Kerr <kerrjoshua@gmail.com>
2023-01-06 16:00:36 +0900

    Got API call working. Started header styling.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
+import Img from './Img';
+import Info from './Info';
 import "./App.css";
 
 function App() {
+
+  const [ photoData, setPhotoData] = useState({});
+
+  useEffect(() => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(res => {setPhotoData(res.data)})
+    .catch(err => console.error(err))
+
+  },[])
+ 
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <div className="App-header"></div>
+      <img src={photoData.url} alt="a daily astronomy photo" />
+      
     </div>
   );
 }
---
+++ src/Img.js
@@ -0,0 +1,3 @@
+
+function Img() {};
+export default Img
