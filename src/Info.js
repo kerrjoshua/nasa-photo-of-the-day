d975428568d88a278277ab9413332464f08a2110
Joshua Kerr <kerrjoshua@gmail.com>
2023-01-10 17:08:09 +0900

    Added some simple styling to the Info component using styled-components.

--- src/Info.js
+++ src/Info.js
@@ -1,23 +1,36 @@
-import { getByTitle } from "@testing-library/react";
+import styled from 'styled-components';
+import './App.css';
+
+const StyledInfo = styled.div`
+border: solid gainsboro 2px;
+border-radius: 3px;
+color: white;
+    background-color: #282c34;
+    & div, p{
+    text-align: left;
+    margin: 10px 15px;
+   }
+   & p {
+    text-indent: 2em;
+    color: dodgerblue;
+   }
+
+`
 
 function Info(props) {
-/**
- * title
- * desc
- * copyright
- * date
- */
+
+
 const {title, desc, copyright, date } = props;
 
 
 
     return (
-        <div>
-            <div><h2>{title}</h2></div>
-            <div>{desc}</div>
-            <div>{date}</div>
-            <div>{copyright}</div>
-        </div>
+        <StyledInfo>            
+            <h2>{title}</h2>
+            <p>Description: {desc}</p>
+            <div>Date: {date}</div>
+            <div>Copyright: {copyright}</div>
+        </StyledInfo>
     );
 };
 
